fe7dad9d76e99f8685faf1b07a99aa39dee15691
dtext <textores.danny@gmail.com>
2021-01-13 21:57:18 +0100

    implement charge monitor

---
+++ app/charge.ts
@@ -0,0 +1,23 @@
+import { battery } from "power";
+
+export class ChargeSensor {
+  constructor(
+    private label: Element,
+  ) {
+    this.setChargeLevel(battery.chargeLevel)
+  }
+
+  private setChargeLevel(chargeLevel: number) {
+    if (chargeLevel == 100) {
+      this.label.text = "9"
+    } else {
+      this.label.text = (chargeLevel / 10).toString()
+    }
+  }
+
+  public start() {
+    battery.addEventListener("change", () => {
+      this.setChargeLevel(battery.chargeLevel)
+    })
+  }
+}
--- app/index.ts
+++ app/index.ts
@@ -2,6 +2,7 @@ import document from "document";
 import clock from "clock";
 import { AlmostMilitaryClock, LOCALE_DE } from "./clock";
 import { HeartRateMonitor } from "./pulse";
+import { ChargeSensor } from "./charge";
 
 
 const timeLabel = document.getElementById("time")
@@ -19,3 +20,7 @@ clock.addEventListener("tick", myClock.updateTime)
 const heartRateLabel = document.getElementById("heart-rate")
 const hrm = new HeartRateMonitor(heartRateLabel)
 hrm.startMonitoring()
+
+const chargeLabel = document.getElementById("charge-level")
+new ChargeSensor(chargeLabel).start()
+
